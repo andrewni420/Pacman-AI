5c365f7898c909f5863da1661444eb345325ca82
Andrew Ni <hickory62133@gmail.com>
2023-12-02 17:12:10 -0500

    initialize q learning agent

--- myTeam.py
+++ myTeam.py
@@ -23,6 +23,8 @@ from baselineTeam import ReflexCaptureAgent
 from util import nearestPoint
 from itertools import product
 import numpy as np 
+from qlearningAgent import PacmanQAgent
+
 
 DEFAULT_DISTANCE=float('inf')
 
@@ -364,3 +366,54 @@ class Agent1(DummyAgent):
             # 'nearest_capsule':10,
             'pacman_ghost':10
             }
+  
+
+
+class FeatureQAgent(PacmanQAgent, Agent1):
+    """
+       ApproximateQLearningAgent
+       You should only have to overwrite getQValue
+       and update.  All other QLearningAgent functions
+       should work as is.
+    """
+    def __init__(self, index, timeForComputing = .1, parser:Parser=None, reward:Reward=None, **kwargs):
+        self.featExtractor = self.getFeatures
+        PacmanQAgent.__init__(self, **kwargs)
+        Agent1.__init__(self, index, timeForComputing=timeForComputing, parser=parser, reward=reward)
+        self.weights = util.Counter()
+
+    def getWeights(self):
+        return self.weights
+
+    def getQValue(self, state, action):
+        """
+          Should return Q(state,action) = w * featureVector
+          where * is the dotProduct operator
+        """
+
+        features = self.featExtractor.getFeatures(state,action)
+        return sum([features[f]*self.weights[f] for f in features])
+
+
+    def update(self, state, action, nextState, reward: float):
+        """
+           Should update your weights based on transition
+        """
+
+        difference = reward+self.discount*self.computeValueFromQValues(nextState) - self.getQValue(state,action)
+        features = self.featExtractor.getFeatures(state,action)
+
+        for f in features.keys():
+            self.weights[f]+=self.alpha*difference*features[f]
+
+
+    def final(self, state):
+        """Called at the end of each game."""
+        # call the super-class final method
+        PacmanQAgent.final(self, state)
+
+        # did we finish training?
+        if self.episodesSoFar == self.numTraining:
+            # you might want to print your weights here for debugging
+            "*** YOUR CODE HERE ***"
+            pass
